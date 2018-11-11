f3bd54dadad67f8d1131dc221dd86c3c24eb4634
SueriGemuese <33549648+SueriGemuese@users.noreply.github.com>
2018-11-11 20:53:02 +0100

    Changed Business Year Cancellation Month from 9 to 12

--- my_sueri/settings.py
+++ my_sueri/settings.py
@@ -185,7 +185,7 @@ DEPOT_LIST_OVERVIEWS = 'x'
 DEPOT_LIST_GENERATION_DAYS = [1,2,3,4,5,6,7]	
 BILLING = False
 BUSINESS_YEAR_START = {"day":1, "month":1}
-BUSINESS_YEAR_CANCELATION_MONTH = 9
+BUSINESS_YEAR_CANCELATION_MONTH = 12
 IMAGES = {'status_100': '/static/img/status_100.png',
             'status_75': '/static/img/status_75.png',
             'status_50': '/static/img/status_50.png',
